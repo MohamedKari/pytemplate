0bb0b0fa3c9c7928ee6c906dbf4c2183873d9c32
Mohamed Kari <contact@mkari.de>
2020-04-05 12:23:14 +0200

    add PEPs

--- PKG_NAME/app.reasoning.py
+++ PKG_NAME/app.reasoning.py
@@ -7,13 +7,17 @@
 # script mode:
 # - executing a module with python some_module.py
 
+# Relevant PEPs
+# https://www.python.org/dev/peps/pep-0328/
+# https://www.python.org/dev/peps/pep-0338/
+
 # RELATIVE IMPORTS: works in package mode
 # --
 # from . import conf
 # from .utils import log
 
 # ABSOLUTE IMPORT relative to the PKG_NAME parent, i. e. imports have a leading "PKG_NAME."
-# works in package mode because when invoking python -m PKG_NAME, the current director is added to the path (https://www.python.org/dev/peps/pep-0338/)
+# works in package mode because when invoking python -m PKG_NAME, the current director is added to the path (see PEP0338)
 # doesn't work in script mode (even with pythin PKG_NAME/app.py), becuase the current directory
 # --
 # from PKG_NAME import conf
@@ -21,7 +25,7 @@
 
 # ABSOLTE IMPORTS and PATH MODIFICATION: relative to the PKG_NAME parent, i. e. imports have a leading "PKG_NAME."
 # --
-# works in package mode because when invoking python -m PKG_NAME, the current director is added to the path (https://www.python.org/dev/peps/pep-0338/)
+# works in package mode because when invoking python -m PKG_NAME, the current director is added to the path (see PEP0338)
 # works in script mode and in package mode
 import sys, os; sys.path.insert(0, os.getcwd())
 from PKG_NAME import conf
